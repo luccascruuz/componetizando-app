a2bd3c143381330aca161e7a827e9fc0517dede4
luccascruuz <luccas.185@hotmail.com>
2021-12-01 14:41:48 -0300

    feat: componentes criados content e sidebar

--- src/components/Content.tsx
+++ src/components/Content.tsx
@@ -1,3 +1,50 @@
-export function Content() {
+import { useEffect, useState } from "react";
+import { GenreResponseProps, IContentProps, MovieProps } from "../@types";
+import { api } from "../services/api";
+import { MovieCard } from "./MovieCard";
+
+export function Content({ selectedGenreId }: IContentProps) {
   // Complete aqui
+  const [movies, setMovies] = useState<MovieProps[]>([]);
+  const [selectedGenre, setSelectedGenre] = useState<GenreResponseProps>(
+    {} as GenreResponseProps
+  );
+
+  useEffect(() => {
+    api
+      .get<MovieProps[]>(`movies/?Genre_id=${selectedGenreId}`)
+      .then((response) => {
+        setMovies(response.data);
+      });
+
+    api
+      .get<GenreResponseProps>(`genres/${selectedGenreId}`)
+      .then((response) => {
+        setSelectedGenre(response.data);
+      });
+  }, [selectedGenreId]);
+
+  return (
+    <div className="container">
+      <header>
+        <span className="category">
+          Categoria:<span> {selectedGenre.title}</span>
+        </span>
+      </header>
+
+      <main>
+        <div className="movies-list">
+          {movies.map((movie) => (
+            <MovieCard
+              key={movie.imdbID}
+              title={movie.Title}
+              poster={movie.Poster}
+              runtime={movie.Runtime}
+              rating={movie.Ratings[0].Value}
+            />
+          ))}
+        </div>
+      </main>
+    </div>
+  );
 }
